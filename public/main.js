93218c7735047d9b18efbe15b7351974f79654bb
Amanda Leaders <alsleaders@gmail.com>
2019-05-16 15:22:52 -0400

    font awesome

--- public/main.js
+++ public/main.js
@@ -1,7 +1,21 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const goGetTheImage = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(response => {
+      return response.json()
+    })
+    .then(message => {
+      console.log(message)
+      console.log(message.url)
+      document.querySelector('.picture-of-the-day').style.backgroundImage =
+        'url(' + message.url + ')'
+      document.querySelector('.copyright').textContent = message.copyright
+      document.querySelector('.image-title').textContent = message.title
+    })
+  // .then(message.url => {
+  //   console.log({ message.url })
+  // })
 }
-
+const main = () => {
+  goGetTheImage()
+}
 document.addEventListener('DOMContentLoaded', main)
